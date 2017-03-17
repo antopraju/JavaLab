40ed2d26520f98fde8212ed97b830323d0e3f30b
ANTHONY PRAJWAL P <anthonyprajwal11@gmail.com>
2017-03-17 22:30:45 +0530

    Add files via upload

---
+++ Program3/TestResultDetails.java
@@ -0,0 +1,13 @@
+import ResultPackage.StudentPackage.RegisterStudent;
+import ResultPackage.ResultDetails;
+
+public class TestResultDetails {
+	public static void main (String args[] ) {
+		
+
+		ResultDetails rd = new ResultDetails();
+		rd.enterGrade();
+		rd.display();
+	}
+}
+
